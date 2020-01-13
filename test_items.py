a8b992ec9f3d8ff18d64de885035043381a48ccf
Dmitry Plevkov <dmitry.plevkov@in-aim.ru>
2020-01-13 23:41:29 +0300

    Add test for add to cart button

---
+++ test_items.py
@@ -0,0 +1,8 @@
+import time
+
+
+def test_add_to_cart_button(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    add_button = browser.find_elements_by_css_selector('button.btn-add-to-basket')
+    assert len(add_button) == 1
+    time.sleep(10)
