234943e643bf2e69d63f99dda5332cce78de3f55
Dmitry Plevkov <dmitry.plevkov@in-aim.ru>
2020-01-13 23:41:46 +0300

    Remove time.sleep

--- test_items.py
+++ test_items.py
@@ -5,4 +5,3 @@ def test_add_to_cart_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     add_button = browser.find_elements_by_css_selector('button.btn-add-to-basket')
     assert len(add_button) == 1
-    time.sleep(10)
